2f32d3da1d8818e8b65f7e8414c286b820b52913
marcho setiawan <dragonmarcho07@gmail.com>
2021-02-17 17:51:45 -0600

    Add project files.

---
+++ GUImodel2/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GUImodel2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+        public ActionResult Overview()
+        {
+            ViewBag.Message = "Your Overview.";
+
+            return View();
+        }
+        public ActionResult SubmitPR()
+        {
+            ViewBag.Message = "Perfomance Rate Form Submission.";
+
+            return View();
+        }
+        public ActionResult RankStructure()
+        {
+            ViewBag.Message = "Your Rank Structure.";
+
+            return View();
+        }
+        public ActionResult History()
+        {
+            ViewBag.Message = "Your Overview.";
+
+            return View();
+        }
+    }
+}
---
+++ GUImodel2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GUImodel2.MvcApplication" Language="C#" %>
